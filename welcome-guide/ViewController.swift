355759da4ff6261390ca284fcebf528133af3895
Philip Heinser <philipheinser@me.com>
2015-06-27 16:03:20 +0200

    Initial commit

---
+++ welcome-guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  welcome-guide
+//
+//  Created by Philip Heinser on 27.06.15.
+//  Copyright (c) 2015 Philip Heinser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
