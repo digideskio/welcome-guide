66b4221a0b09b488b0ece742b2741737d79bc288
Philip Heinser <philipheinser@me.com>
2015-06-28 09:50:09 +0200

    added chat view controller

---
+++ welcome-guide/ChatViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatViewController.swift
+//  
+//
+//  Created by Philip Heinser on 28.06.15.
+//
+//
+
+import UIKit
+
+class ChatViewController: JSQMessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.senderId = "123";
+        self.senderDisplayName = "test";
+        title = "Chat"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- welcome-guide/welcome-guide-BridgingHeader.h
+++ welcome-guide/welcome-guide-BridgingHeader.h
@@ -11,4 +11,5 @@
 
 #import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
+#import "JSQMessages.h"
 #endif
